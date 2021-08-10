b4302bdc54ae74bf9e7f641d0500efe72c5718aa
maskeynihal <maskeynihal@gmail.com>
2021-08-10 17:42:53 +0545

    Update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,23 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+  ],
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config) => {
+    // add SCSS support for CSS Modules
+    config.module.rules.push({
+      test: "/.s[ca]ss$/",
+      use: ["style-loader", "css-loader?modules&importLoaders", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
 };
